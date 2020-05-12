24bd2fd33158106f86267bc787ab35bcfcc2bbef
Jacob Gunderson <jgunde@earthlink.net>
2020-05-12 11:11:37 -0700

    Add files via upload

---
+++ RPiPCA9685/__init__.py
@@ -0,0 +1,9 @@
+"""
+This is a package used to control the PCA9685 16 Channel 12-bit PWM driver with a Raspberry pi.
+See the examples folder for usage examples.
+"""
+
+__version__ = "1.0"
+__author__ = "Jacob Gunderson"
+__url__ = "https://github.com/Jgunde/RPiPCA9685"
+
